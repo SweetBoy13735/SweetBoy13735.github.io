e7b39313fd42c15e85de1ec943c72b0bfeff1229
Ramone Graham <ramonegraham@gmail.com>
2024-02-13 20:43:41 +0000

    Create NavBar responsive scripting
    - Added the responsive CSS required for the NavBar.
    - Added the JS required for the NavBar responsive functionality.
    - Added the CSS required for the button and ButtonLink component.

--- res/js/app.js
+++ res/js/app.js
@@ -1,7 +1,20 @@
 //#region DOM elements
-const navBar = document.querySelector("nav.bar");
+const navBar = document.querySelector("nav.bar"), navBarMenu = navBar.querySelectorAll("li"), navBarToggle = navBar.querySelector(".toggle");
+//#endregion
+
+//#region State management
+let navBarMenuVisible = false;
+
+function setNavBarMenuVisibility(visible) {
+	navBarMenuVisible = visible;
+
+	navBarMenu.forEach(option => { if (option !== navBarMenu.item(0)) option.style.height = navBarMenuVisible ? "1.25rem" : "0"; });
+}
 //#endregion
 
 //#region Event listeners
 window.addEventListener("scroll", () => { navBar.classList.toggle("scrolled", window.scrollY > 0); });
+
+navBarToggle.onclick = () => { setNavBarMenuVisibility(!navBarMenuVisible); };
+navBarMenu.forEach(option => { option.onclick = () => { setNavBarMenuVisibility(false); }; });
 //#endregion
