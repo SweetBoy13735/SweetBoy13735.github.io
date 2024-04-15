314a88821a54edafaa3ce9e45967259011c55af5
Ramone Graham <ramonegraham@gmail.com>
2024-04-15 13:12:47 +0100

    Reset workspace
    Replaced all existing files with my custom boilerplate.

--- res/js/app.js
+++
@@ -1,6 +0,0 @@
-var navMenuVisible = false, navMenu = document.getElementsByClassName("nav-menu")[0];
-
-function toggleNavMenu() {
-    navMenu.style.right = navMenuVisible ? "-151.86px" : "25px";
-    navMenuVisible = !navMenuVisible;
-}
